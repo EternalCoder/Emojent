d23f8586ef4cb8dfdaa78895b3283c983a92ee24
Wilson Burchenal <Wilson Burchenal>
2017-05-04 21:05:33 -0700

    Minor Code Changes

--- SourceCode/app/src/main/java/com/emojent/emojent/MainScreenActivity.java
+++ SourceCode/app/src/main/java/com/emojent/emojent/MainScreenActivity.java
@@ -1,13 +1,23 @@
 package com.emojent.emojent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Test", "OnCreation");
         setContentView(R.layout.activity_main_screen);
+    }
+
+    public void activityRecognize(View v) {
+        Log.d("Test", "Activity Recognize function");
+        Intent intent = new Intent(this, VideoScreenActivity.class);
+        startActivity(intent);
     }
 }
