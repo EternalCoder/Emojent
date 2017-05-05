f28b948f2013a7162b3401921cfa6d92d43fcb92
Kevin Boehnlein <kevin.boehnlein@gmail.com>
2017-05-04 23:28:08 -0700

    Code cleanup and other changes
    Finalized activity switching, updated the minimum SDK version from 19 to
    21, and added dictionary words(!!!)

--- SourceCode/app/src/main/java/com/emojent/emojent/VideoScreenActivity.java
+++ SourceCode/app/src/main/java/com/emojent/emojent/VideoScreenActivity.java
@@ -1,25 +1,11 @@
 package com.emojent.emojent;
 
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.net.Uri;
 import android.util.Log;
-import android.widget.VideoView;
-import android.content.Context;
-
-import com.microsoft.projectoxford.emotion.EmotionServiceClient;
-import com.microsoft.projectoxford.emotion.EmotionServiceRestClient;
-import com.microsoft.projectoxford.emotion.contract.FaceRectangle;
-import com.microsoft.projectoxford.emotion.contract.RecognizeResult;
-import com.microsoft.projectoxford.emotion.rest.EmotionServiceException;
 //import com.microsoft.projectoxford.emotionsample.helper.ImageHelper;
 
-
 public class VideoScreenActivity extends AppCompatActivity {
-
-
     //android:onClick="VideoScreenActivity"
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +18,9 @@ public class VideoScreenActivity extends AppCompatActivity {
     static final int REQUEST_VIDEO_CAPTURE = 1;
     private void dispatchTakeVideoIntent() {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != NULL) {
             startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
         }
     }
     */
-
 }
